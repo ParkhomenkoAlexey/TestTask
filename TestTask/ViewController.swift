e140d96f4a65fdc67295650e3d5d4806bebfe65a
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-11-13 17:58:47 +0300

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Алексей Пархоменко on 13.11.2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
